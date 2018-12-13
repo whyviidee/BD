689adf870b88f306d77bd2e5c57ff1a7afd50114
ftrindadesantos <32799563+ftrindadesantos@users.noreply.github.com>
2018-12-13 21:18:17 +0000

    Mudança

--- PROJETO BD/4Entrega/Tables.sql
+++ PROJETO BD/4Entrega/Tables.sql
@@ -40,7 +40,7 @@ CREATE TABLE fact_table(
 -- POPULATE DAS TABELAS
 
 
-INSERT INTO d_evento (numTelefone, instanteChamada) 
+INSERT INTO d_evento (numTelefone, instanteChamada)
 	SELECT numTelefone, instanteChamada
 		FROM eventoEmergencia;
 
@@ -55,7 +55,7 @@ INSERT INTO d_tempo (dia, mes, ano)
 		   EXTRACT (YEAR FROM instanteChamada) as ano
 		   FROM eventoEmergencia;
 
-INSERT INTO fact_table(idEvento, idMeio, idData)	
+INSERT INTO fact_table(idEvento, idMeio, idData)
 		SELECT idEvento ,idMeio, idData FROM d_evento , d_meio ,d_tempo ;
 
 
@@ -63,5 +63,44 @@ INSERT INTO fact_table(idEvento, idMeio, idData)
 
 -- RESTRICOES DE INTEGRIDADE
 
+DROP TRIGGER IF EXISTS chk_solicita_trigger AS solicita;
+
+CREATE OR REPLACE FUNCTION chk_solicita() returns Trigger AS $$
+BEGIN
+  DECLARE n int;
+  SET n = (
+    SELECT COUNT(idCoordenador)
+    FROM vigia NATURAL JOIN eventoEmergencia NATURAL JOIN audita AS table
+    WHERE new.idCoordenador = table.idCoordenador AND new.numCamara = table.numCamara
+    );
+  IF n < 1 THEN
+    RAISE EXCEPTION 'Nonexistent Coordinator ID',
+    USING HINT = 'Check if Coordinator auditates the local adress'
+  END if;
+end;
+$$ Language plpgsql;
+
+CREATE TRIGGER chk_solicita_trigger after update on solicita
+FOR EACH ROW EXECUTE PROCEDURE chk_solicita();
 
 
+
+DROP TRIGGER IF EXISTS chk_alocado_trigger AS alocado;
+
+CREATE OR REPLACE FUNCTION chk_alocado() returns Trigger AS $$
+BEGIN
+  DECLARE n int;
+  SET n = (
+    SELECT COUNT(numMeio)
+    FROM meioApoio NATURAL JOIN Accionado AS table
+    WHERE new.numMeio = table.numMeio AND new.numProcessoSocorro= table.numProcessoSocorro
+    );
+  IF n < 1 THEN
+    RAISE EXCEPTION 'Nonexistent Half',
+    USING HINT = 'Check if Half alocaded to the HelpProcess'
+  END if;
+end;
+$$ Language plpgsql;
+
+CREATE TRIGGER chk_alocado_trigger after update on alocado
+FOR EACH ROW EXECUTE PROCEDURE chk_alocado();
